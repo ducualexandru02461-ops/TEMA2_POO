69633e72a4367c075028d0373c51fd6afa6ae90c
hammer_02461 <ducualexandru02461@gmail.com>
2026-05-01 18:38:07 +0300

    Update proiect2.h

--- proiect2.h
+++ proiect2.h
@@ -4,105 +4,163 @@
 #include <vector>
 #include <string>
 
-class Persoana{
+///PERSOANA///
+
+class Persoana {
 protected:
     std::string nume;
-    std::string prenume;
 public:
-    Persoana(const std::string& nume_) : nume(nume_) {}
-    ~Persoana(){}
+    Persoana() {}
+    Persoana(const std::string& n) : nume(n) {}
+    std::string getNume() const { return nume; }
+    virtual void citire (std::istream& in) = 0;
+    virtual void afisare() const = 0;
+    virtual ~Persoana() {}
 };
 
-class Student : public Persoana{
-    int anDeStudiu;
-    int grupa;
-    char* specializare;
+///STUDENT///
+
+class Student : public Persoana {
 public:
-    //Student (){
-    //    std::cin >> this->nume;
-    //}
-    Student () : Persoana("") {
-        std::cout << "-Nume: ";
-        std::getline(std::cin, nume);
+    Student() {}
+    Student(const std::string& n) : Persoana(n) {}
+
+    void citire(std::istream& in) override {
+        std::cout << "Nume student: ";
+        std::getline(in, nume);
     }
-    std::string getNume() const{
-        return nume;
+
+    void afisare() const override {
+        std::cout << "Student: " << nume << "\n";
     }
-    ~Student(){}
 };
 
-class Curs{
-protected:
+///CURS///
+
+class Curs {
     std::string nume;
-    int credite = 0;
+    int credite;
 public:
-    //Curs(const std::string& nume_){ nume = nume_; }
-    //std::string getNume() const{
-    //   return numeCurs;
-    //}
-    void citire() {
-        std::cout << "-Nume curs: ";
-        std::getline(std::cin, nume);
-    }
-    std::string getNume() const {
-        return nume;
-    }
-    ~Curs(){}
-};
+    Curs() {}
+    Curs(const std::string& n, int c) : nume(n), credite(c) {}
 
-class Credite{
-    std::string credite;
-public:
-    void citire(){
-        std::cout << "-Credite: ";
-        std::cin >> credite;
-    }
-    std::string getCredite() const {
-        return credite;
+    std::string getNume() const { return nume; }
+    int getCredite() const { return credite; }
+
+    void citire(std::istream& in){
+        std::cout << "Nume curs: ";
+        std::getline(in, nume);
+
+        std::cout << "Credite: ";
+        in >> credite;
+        in.ignore();
     }
 };
 
-class Profesor : public Persoana{;
+///PROFESOR///
+
+class Profesor : public Persoana {
     Curs curs;
+
 public:
-    Profesor () : Persoana("") {
-        std::cout << "-Nume profesor: ";
-        std::getline(std::cin, nume);
+    Profesor() {}
+    Profesor(const std::string& n, const Curs& c)
+        : Persoana(n), curs(c) {}
+
+    std::string getNumeCurs() const { return curs.getNume(); }
+    int getCrediteCurs() const { return curs.getCredite(); }
+
+    void citire(std::istream& in) override {
+        std::cout << "Nume profesor: ";
+        std::getline(in, nume);
+
+        curs.citire(in);
     }
-    std::string getNume() const{
-        return nume;
+
+    void afisare () const override {
+        std::cout << "Profesor:" << nume
+                  << " | " << curs.getNume()
+                  << " | " << curs.getCredite() << "\n";
+
     }
-    std::string getNumeCurs() const{
-        return curs.getNume();
-    }
-    /*int getCrediteCurs() const{
-        return curs.getCredite();
-    }*/
-    void setCurs(const Curs& c){
-        curs = c;
-    }
-    ~Profesor(){}
 };
 
-class Facultate{
+///SECRETAR///
+
+class Secretar : public Persoana {
+    std::string departament;
+    int vechime;
+
+public:
+    Secretar() : Persoana(""), departament(""), vechime(0) {}
+
+    Secretar(const std::string& nume_, const std::string& dep, int v)
+        : Persoana(nume_), departament(dep), vechime(v){}
+
+    std::string getDepartament() const{
+        return departament;
+    }
+
+    int getVechime() const{
+        return vechime;
+    }
+
+    void afisare() const {
+        std::cout << "Secretar: " << nume
+                  << " | Departament: " << departament
+                  << " | Vechime: " << vechime << " ani\n";
+    }
+
+    void citire(std::istream& in) override{
+        std::cout << "Nume secretar: ";
+        std::getline(in, nume);
+
+        std::cout << "Departament: ";
+        std::getline(in, departament);
+
+        std::cout << "Vechime: ";
+        in >> vechime;
+        in.ignore();
+    }
+};
+
+///FACULTATE///
+
+class Facultate {
     std::string nume;
     std::string oras;
-    std::vector<Student> listaStudenti;
-    std::vector<Profesor> listaProfesori;
-    std::vector<Curs> listaCursuri;
 public:
-    void setNume() {std::getline(std::cin, nume);}
-    void setOras() {std::getline(std::cin,oras);}
-    std::string getNume() const { return nume; }
-    std::string getOras() const { return oras; }
-    ~Facultate(){}
+    std::vector<Student> studenti;
+    std::vector<Profesor> profesori;
+    std::vector<Secretar> secretari;
+
+    void setDate(const std::string& n, const std::string& o) {
+        nume = n;
+        oras = o;
+    }
+
+    void afisare() {
+        std::cout << "Facultate: " << nume << "\n";
+        std::cout << "Oras: " << oras << "\n\n";
+
+        std::cout << "Studenti:\n";
+        for (auto& s : studenti)
+            std::cout << "- " << s.getNume() << "\n";
+
+        std::cout << "\nProfesori:\n";
+        for (auto& p : profesori) {
+            std::cout << "- " << p.getNume()
+                      << " | " << p.getNumeCurs()
+                      << " | " << p.getCrediteCurs() << "\n";
+        }
+
+        std::cout << "\nSecretari:\n";
+        for (auto& s : secretari) {
+            std::cout << "- " << s.getNume()
+                      << " | " << s.getDepartament()
+                      << " | " << s.getVechime() << "\n";
+        }
+    }
 };
 
-
-/*void afisareOptiuni () {
-    std::cout << "Apasati:" <<"\n";
-    std::cout << "-> 0, pentru a intrerupe crearea bazei de date" << "\n";
-    std::cout << "-> 1, pentru a continua crearea bazei de date" << "\n";
-}*/
-
-#endif // PROIECT2_H_INCLUDED
+#endif
